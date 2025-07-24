b3b373ecf794a1ef388ddd6adbfd1bae0913ee1c
swapnilkambleit <swapnil.kamble.it@gmail.com>
2025-07-24 14:58:10 +0530

    laravel acl

---
+++ database/seeders/CreateAdminUserSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+  
+namespace Database\Seeders;
+  
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+  
+class CreateAdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+ 
+        $user = User::create([
+            'name' => 'Hardik Savani', 
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin@123')
+        ]);
+        $role = Role::create(['name' => 'Admin']);
+         
+        $permissions = Permission::pluck('id','id')->all();
+       
+        $role->syncPermissions($permissions);
+         
+        $user->assignRole([$role->id]);
+
+        $roles = [
+          //'Admin',
+           'Staff', 
+           'Editor', 
+        ];
+        
+        foreach ($roles as $role) {
+            Role::create(['name' => $role]);
+        }
+
+        /*
+        $role = Role::create(['name' => 'Admin']);
+        $permissions = Permission::pluck('id','id')->all();
+        $role->syncPermissions($permissions);
+        $user->assignRole([$role->id]);
+        */
+
+    }
+}
